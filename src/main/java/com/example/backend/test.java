6cb4b9540fe512a20be51e2ccc1aa8db4af1cbad
Dusker233 <1187305740@qq.com>
2023-12-22 16:43:53 +0800

    20231222-getData

--- src/main/java/com/example/backend/test.java
+++ src/main/java/com/example/backend/test.java
@@ -1,23 +1,43 @@
 package com.example.backend;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.backend.po.Content;
+import com.example.backend.po.Values;
+import com.example.backend.utils.AmqpClient;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 @RestController
 public class test {
-    @RequestMapping("/toJson")
-    @ResponseBody
-    public Map<String, String> toJson(){
 
-        Map<String,String> map= new HashMap<String,String>();
-        map.put("name","wang");
+    AmqpClient amqpClient = new AmqpClient();
 
+    @GetMapping("/getData")
+    public Map<String, String> getData() {
+        while(amqpClient.getContentSet().isEmpty()) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        // 取出 set 中最后插入的元素
+        Content latestContent = amqpClient.getContentSet().iterator().next();
+        Map<String, String> map = new HashMap<>();
+        for(Map.Entry<String, Values> entry : latestContent.getItems().entrySet()) {
+//            System.out.println(entry.getKey() + " : " + entry.getValue().getValue());
+            map.put(entry.getKey(), entry.getValue().getValue());
+        }
         return map;
+    }
 
+    @PostMapping("/sendData")
+    public void sendData(@RequestParam String params) {
+        // TODO: jsonify the params
+        // TODO: transform it to Map<String, String> or sth else
+//        amqpClient.sendMessage(params);
     }
 
 }
